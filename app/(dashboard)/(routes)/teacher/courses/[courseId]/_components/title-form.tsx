06c9bf16dfa63e76832c304fb386bb04b5d10193
congtc <congtc.dev@gmail.com>
2023-11-25 19:11:15 +0700

    done uploadthing and image form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -4,67 +4,93 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
-  FormLabel,
-  FormMessage,
   FormItem,
+  FormMessage,
 } from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+};
 
 const formSchema = z.object({
   title: z.string().min(1, {
-    message: "Tiêu đề bắt buộc",
+    message: "Tiêu đề không được để trống",
   }),
 });
 
-const CreatePage = () => {
+export const TitleForm = ({
+  initialData,
+  courseId
+}: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
   const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-    },
+    defaultValues: initialData,
   });
 
   const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("/api/courses", values);
-      router.push(`/teacher/courses/${response.data.id}`);
-      toast.success("Tạo khoá học thành công");
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Khoá học đã được cập nhật");
+      toggleEdit();
+      router.refresh();
     } catch {
       toast.error("Có lỗi xảy ra!");
     }
-  };
+  }
 
   return (
-    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
-      <div>
-        <h1 className="text-2xl">Tên khoá học</h1>
-        <p className="text-sm text-slate-600">
-          Bạn có chắc là muốn sử dụng tên này chứ? Oki, tôi sẽ sửa lại sau.
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Tiêu đề khoá học
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Huỷ</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Chỉnh sửa tiêu đề
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className="text-sm mt-2">
+          {initialData.title}
         </p>
+      )}
+      {isEditing && (
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8 mt-8"
+            className="space-y-4 mt-4"
           >
             <FormField
               control={form.control}
               name="title"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Tên khoá học</FormLabel>
                   <FormControl>
                     <Input
                       disabled={isSubmitting}
@@ -72,26 +98,21 @@ const CreatePage = () => {
                       {...field}
                     />
                   </FormControl>
-                  <FormDescription>Mô tả khoá học</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
             <div className="flex items-center gap-x-2">
-              <Link href="/">
-                <Button type="button" variant="ghost">
-                  Huỷ
-                </Button>
-              </Link>
-              <Button type="submit" disabled={!isValid || isSubmitting}>
-                Tiêp tục
+              <Button
+                disabled={!isValid || isSubmitting}
+                type="submit"
+              >
+                Lưu
               </Button>
             </div>
           </form>
         </Form>
-      </div>
+      )}
     </div>
-  );
-};
-  
-export default CreatePage;
+  )
+}
