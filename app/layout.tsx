ad9cf7de1b52cffc6c9ff60da202671f197de0cb
congtcdev <congtc.dev@gmail.com>
2024-03-16 18:41:57 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
   description: "congtcdev - Dạy lập trình",
   icons: "./icon.svg",
   openGraph: {
-    title: 'congtcdev',
-    description: 'Website dạy lập trình miễn phí.',
-    images: ['https://raw.githubusercontent.com/tranchiencongtd/hosting-file/main/images/logo.png'],
+    title: 'congtcdev - Học lập trình miễn phí',
+    description: 'Website dạy lập trình miễn phí. Học lập trình qua dự án thực tế cho các bạn học viên IT. Cung cấp các khoá học chất lượng giúp các bạn tăng kỹ năng và kiến thức trong nghề.',
+    images: ['https://raw.githubusercontent.com/tranchiencongtd/hosting-file/main/images/logo2.png'],
     type: "website"
   }
 };
